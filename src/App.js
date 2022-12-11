4375addcad642c8fdf83005722e590d1d8c2cbcf
Olechka31 <Olechka31@users.noreply.github.com>
2022-12-11 14:30:38 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <p className="title">DICTIONARY</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
